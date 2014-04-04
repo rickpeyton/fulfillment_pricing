ee97589e0ec90e6afcd2f7077110d517a5430c70
rickpeyton <therickpeyton@gmail.com>
2014-04-04 17:46:22 -0500

    Fulfillment Pricing 20140404

---
+++ item_codes.rb
@@ -0,0 +1,72 @@
+# yc0c88
+card_types = ['Flat','Folded','Tri-Fold','Sticker','Vinyl Sticker']
+back_side  = [0,1,4]
+sizes = {
+  'A' =>  0,
+  'B' =>  1,
+  'C' =>  2,
+  'D' =>  3,
+  'E' =>  4,
+  'F' =>  5,
+  'G' =>  6,
+  'H' =>  7,
+  'I' =>  8,
+  'J' =>  9,
+  'K' => 10,
+  'L' => 11,
+  'M' => 12,
+  'N' => 13,
+  'O' => 14,
+  'P' => 15,
+  'Q' => 16,
+  'R' => 17,
+  'S' => 18,
+  't' =>  0.125,
+  'u' =>  0.250,
+  'v' =>  0.375,
+  'w' =>  0.500,
+  'x' =>  0.625,
+  'y' =>  0.750,
+  'z' =>  0.875
+}
+papers = {
+  '110 Smooth Ultra White' => {
+    :code => '88',
+  }
+  '88 Smooth Soft White' => {
+    :code => 'I8',
+  }
+  '105 Metallic Pearl' => {
+    :code => 'AP',
+  }
+  '110 Felt Bright White' => {
+    :code => 'WW',
+  }
+  '110 Felt Warm White' => {
+    :code => 'WC',
+  }
+  '110 Recycled White' => {
+    :code => 'PCW',
+  }
+  '118 Cotton Brilliant White' => {
+    :code => 'RSW',
+  }
+  '120 Eggshell Ultra White' => {
+    :code => 'UW',
+  }
+  '120 Eggshell White' => {
+    :code => '2E'
+  }
+  'Kiss Cut' => {
+    :code => 'ST'
+  }
+  'Crack & Peel' => {
+    :code => 'GC'
+  }
+  'Kiss Cut' => {
+    :code => 'VS'
+  }
+  'Crack & Peel' => {
+    :code => 'VC'
+  }
+}
